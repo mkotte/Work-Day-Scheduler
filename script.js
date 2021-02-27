6f7bd84c97cc46ccfb10d5a0a667744f65c4b98e
mkotte <74936870+mkotte@users.noreply.github.com>
2021-02-26 21:28:11 -0500

    started on js, planned out steps

---
+++ script.js
@@ -0,0 +1,12 @@
+// declaring + appending the current date using moment.js
+var currentDay = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").text(currentDay); 
+
+// make for loop to create planner rows
+    //with container element (div?),  containing time, textarea + save button
+
+// add save button listener w/ local storage
+
+// create function where color coincides with time of day
+
+// on page load, pull data from local storage (if there is data in local storage) 
