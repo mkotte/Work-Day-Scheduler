8007bd9e30d92268fc9f1e9eac7e8f406290e7a3
mkotte <74936870+mkotte@users.noreply.github.com>
2021-02-26 23:36:31 -0500

    rows appearing, starting to shape up

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ for ( let i=9 ; i < 18 ; i++ ){
     var container = $('.container')
     var plannerRow = $('<div>');
     plannerRow.addClass('row');
-    plannerRow.attr('id', ( i + ":00" ))
-    plannerRow.appendTo(container)
+    plannerRow.attr('id', ( i + ":00" ));
+    plannerRow.appendTo(container);
     
     //adding the time, textarea + save button
+    var plannerTime = $('<p>');
+    plannerTime.addClass('hour').text(i + ":00");
+    plannerTime.appendTo(plannerRow);
+
+    var textArea = $('<textarea>');
+    textArea.addClass('past').text('');
+    textArea.appendTo(plannerRow);
+
+    var saveBtn = $('<button>');
+    saveBtn.addClass('saveBtn');
+    saveBtn.appendTo(plannerRow);
 
 }
 // add save button listener w/ local storage
